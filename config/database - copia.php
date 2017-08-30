2e7f226e6992e077793f5b5757d013e55fbe9e24
chico1966 <chico1966.basura@gmail.com>
2017-08-30 18:12:01 +0200

    Cambio de Database

--- config/database - copia.php
+++ config/database - copia.php
@@ -12,27 +12,55 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-/*Del archivo .env
-DB_CONNECTION=pgsql
-DB_HOST=ec2-54-247-175-255.eu-west-1.compute.amazonaws.com
-DB_PORT=5432
-DB_DATABASE=df8aj5kus20p0o
-DB_USERNAME=tkkpjsqgqktblo
-DB_PASSWORD=8e37a9fb3d61fa92f51c754e2f5f11c42bf72bee5d48c6cd2dcd19a203c3d60a
-*/
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the database connections setup for your application.
+    | Of course, examples of configuring each database platform that is
+    | supported by Laravel is shown below to make development simple.
+    |
+    |
+    | All database work in Laravel is done through the PHP PDO facilities
+    | so make sure you have the driver for your particular database of
+    | choice installed on your machine before you begin development.
+    |
+    */
 
     'connections' => [
 
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+        ],
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'ec2-54-247-175-255.eu-west-1.compute.amazonaws.com'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'df8aj5kus20p0o'),
-            'username' => env('DB_USERNAME', 'tkkpjsqgqktblo'),
-            'password' => env('DB_PASSWORD', '8e37a9fb3d61fa92f51c754e2f5f11c42bf72bee5d48c6cd2dcd19a203c3d60a'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
